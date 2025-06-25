87d531876c53cf1159f81705e9ee5436f3a81fc0
Anupam Pandey <pandeyanupam180@gmail.com>
2025-06-25 15:55:56 +0530

    Time: 1 ms (79.91%) | Memory: 60.8 MB (25.66%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -3,12 +3,11 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let l = 0;
-    for(let r = 0; r < nums.length; r++){
-        if(nums[r] !== 0){
-            [nums[l], nums[r]] = [nums[r], nums[l]];
-            l++;
-        }
+  let k = 0;
+  for(let i=0; i<nums.length; i++){
+    if(nums[i] != 0){
+        [nums[k], nums[i]] = [nums[i], nums[k]]
+        k++;
     }
-    return nums;
+  }  
 };
