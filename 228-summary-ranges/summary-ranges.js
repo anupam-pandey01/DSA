b54579fac8449e622040db1b8f0e944154f90f0f
Anupam Pandey <pandeyanupam180@gmail.com>
2025-06-25 16:18:20 +0530

    Time: 0 ms (100.00%) | Memory: 53.3 MB (57.50%) - LeetSync

--- 228-summary-ranges/summary-ranges.js
+++ 228-summary-ranges/summary-ranges.js
@@ -3,21 +3,21 @@
  * @return {string[]}
  */
 var summaryRanges = function(nums) {
-  let i = 0;
+  let l = 0;
   let res = [];
-  while(i < nums.length){
-    let start = nums[i];
-
-    while(i < nums.length-1 && (nums[i] + 1 == nums[i + 1])){
-        i += 1;
+  while(l < nums.length){
+    let start = nums[l];
+    
+    while(l < nums.length-1 && (nums[l] + 1 == nums[l + 1])){
+        l+=1;
     }
-    if(start != nums[i]){
-        res.push(String(start) + "->" + String(nums[i]));
+    if(nums[l] == start){
+        res.push(String(start))
     }
     else{
-        res.push(String(nums[i]));
+        res.push(String(start) + "->" + String(nums[l]))
     }
-    i++;
+    l+=1;
   }
   return res;
 };
