c49c133648db544f399c83f8af463ec04a3de738
Anupam Pandey <pandeyanupam180@gmail.com>
2025-06-25 15:45:56 +0530

    Time: 1 ms (99.46%) | Memory: 62.3 MB (54.10%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -5,16 +5,16 @@
 var sortedSquares = function(nums) {
     let left = 0;
     let right = nums.length - 1;
-    let res = []
-    while(left <= right){
-        if(Math.abs(nums[left]) <= Math.abs(nums[right])){
-            res.push(nums[right] ** 2);
-            right--
+    let result = [];
+    while(left <= right){ 
+        if(Math.abs(nums[left]) < Math.abs(nums[right])){
+            result.push(nums[right] ** 2);
+            right--;
         }
         else{
-            res.push(nums[left] ** 2);
-            left ++
+            result.push(nums[left] ** 2);
+            left++;
         }
     }
-    return res.reverse();
+    return result.reverse()
 };
