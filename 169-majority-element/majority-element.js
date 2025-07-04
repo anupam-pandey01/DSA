1c27a37bf826e02a5b249eacc37ffddd59729dc0
Anupam Pandey <pandeyanupam180@gmail.com>
2025-07-04 07:38:47 +0530

    Time: 2 ms (73.25%) | Memory: 56.6 MB (65.70%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,18 +3,18 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-   let freq = 0;
+   let freq = 0 ;
    let ans = 0;
    for(let i=0; i<nums.length; i++){
-        if(freq == 0){
-            ans = nums[i]
-        }
-        if(ans == nums[i]){
-            freq++
-        }
-        else{
-            freq--
-        }
+     if(freq == 0){
+        ans = nums[i]
+     }
+     if(nums[i] == ans){
+        freq++;
+     }
+     else{
+        freq--;
+     }
    }
-    return ans;
+   return ans;
 };
