0851b29e409c58b297f6c8210a1e49d2dcff228d
Anupam Pandey <pandeyanupam180@gmail.com>
2025-07-18 11:10:29 +0530

    Time: 1 ms (96.56%) | Memory: 65.3 MB (31.87%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let  maxProfit = 0;
     let bestBuy = prices[0];
+    let maxprofit = 0;
     for(let i=0; i<prices.length; i++){
         if(prices[i] > bestBuy){
-            maxProfit = Math.max(maxProfit, prices[i] - bestBuy);
+            maxprofit = Math.max(maxprofit, prices[i] - bestBuy)
         }
         bestBuy = Math.min(bestBuy, prices[i]);
     }
-    return maxProfit;
+    return maxprofit;
 };
