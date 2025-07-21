1e875afd5f3d301f06bfae6ccf699ca01239f6db
Anupam Pandey <pandeyanupam180@gmail.com>
2025-07-21 09:15:18 +0530

    Time: 59 ms (18.08%) | Memory: 77.9 MB (63.20%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    let seen = {};
+    for(let i=0; i<nums.length; i++){
+        let val = nums[i];
+        if(seen.hasOwnProperty(val) && i - seen[val] <= k){
+            return true;
+        }
+        seen[val] = i;
+    }
+    return false;
+};
