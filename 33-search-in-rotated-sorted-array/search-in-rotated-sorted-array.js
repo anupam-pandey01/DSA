a329a23e51fa03b5fc5ee10fcbd8c8d9487b7487
Anupam Pandey <pandeyanupam180@gmail.com>
2025-07-24 11:59:00 +0530

    Time: 0 ms (100.00%) | Memory: 53.5 MB (61.35%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -4,9 +4,28 @@
  * @return {number}
  */
 var search = function(nums, target) {
-  for(let i=0; i<nums.length; i++){
-    if(nums[i] == target){
-        return i;
+  let st = 0;
+  let end = nums.length - 1;
+  while(st <= end){
+    let mid = Math.floor((st + end) / 2);
+    if(nums[mid] == target){
+        return mid;
+    }
+
+    if(nums[st] <= nums[mid]){
+        if(nums[st] <= target && target <= nums[mid]){
+            end = mid - 1;
+        }
+        else{
+            st = mid + 1;
+        }
+    }else{
+        if(nums[mid] <= target && target <= nums[end]){
+            st = mid + 1;
+        }
+        else{
+            end = mid - 1;
+        }
     }
   }
   return -1;
