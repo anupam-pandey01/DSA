a35351d9783c02ba7c6e47c71879648d560f8ca4
Anupam Pandey <pandeyanupam180@gmail.com>
2025-08-15 11:54:51 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (90.66%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -6,9 +6,9 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let midx = m-1;
-    let nidx = n-1;
-    let right = m+n-1;
+    let nidx = n - 1;
+    let midx = m - 1;
+    let right = m + n -1;
 
     while(nidx >= 0){
         if(midx >= 0 && nums1[midx] > nums2[nidx]){
