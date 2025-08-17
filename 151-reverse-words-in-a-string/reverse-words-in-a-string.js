edef738feefb12d0dbeb2ac2ce469861e5ab14f6
Anupam Pandey <pandeyanupam180@gmail.com>
2025-08-17 10:48:53 +0530

    Time: 2 ms (61.48%) | Memory: 55.8 MB (40.80%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.js
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.js
@@ -4,13 +4,12 @@
  */
 var reverseWords = function(s) {
     let words = s.split(" ");
-    let res = [];
-
-    for(let i=words.length - 1; i>=0; i--){
-        if(words[i]){
-            res.push(words[i])
+    let res = []
+    for(let i=0; i<words.length; i++){
+        if(words[i] != ""){
+            res.push(words[i]);
         }
     }
-
-    return res.join(" ");
+    let reversed = res.reverse();
+    return reversed.join(" ");
 };
