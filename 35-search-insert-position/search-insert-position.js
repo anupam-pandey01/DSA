01bffe66da46ac38114a124d0824f14cdde48299
Anupam Pandey <pandeyanupam180@gmail.com>
2025-08-22 12:25:43 +0530

    Time: 0 ms (100.00%) | Memory: 53.6 MB (65.78%) - LeetSync

--- 35-search-insert-position/search-insert-position.js
+++ 35-search-insert-position/search-insert-position.js
@@ -4,20 +4,20 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    let left = 0;
-    let right = nums.length - 1;
+    let right=nums.length - 1;
+    let left=0;
 
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-
-        if (nums[mid] === target) {
+    while(left <= right){
+        let mid = Math.floor((right + left)/2);
+        if(nums[mid] == target){
             return mid;
-        } else if (nums[mid] > target) {
-            right = mid - 1;
-        } else {
+        }
+        else if( nums[mid] < target){
             left = mid + 1;
         }
+        else{
+            right = mid - 1;
+        }
     }
-
-    return left;    
+    return left;
 };
