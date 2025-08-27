87e9f7baf964a23f49bc1a2b3bfc57604881f907
Anupam Pandey <pandeyanupam180@gmail.com>
2025-08-27 11:45:36 +0530

    Time: 15 ms (12.63%) | Memory: 62.6 MB (98.93%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -3,17 +3,23 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    if (x < 0) {
-        return false;
-    }
+    if(x < 0) return false;
 
-    let reverse = 0;
-    let xcopy = x;
-
-    while (x > 0) {
-        reverse = (reverse * 10) + (x % 10);
+    const arr = [];
+    while(x > 0){
+        let val = x % 10;
         x = Math.floor(x / 10);
+        arr.push(val);
     }
 
-    return reverse === xcopy;    
+    let left = 0;
+    let right = arr.length - 1;
+    while(left < right){
+        if(arr[left] != arr[right]){
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
 };
