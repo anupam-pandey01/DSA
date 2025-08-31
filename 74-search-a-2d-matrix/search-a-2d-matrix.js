680f4c8fd2c0df8eaf582b6592872a128a44d505
Anupam Pandey <pandeyanupam180@gmail.com>
2025-08-31 11:03:20 +0530

    Time: 0 ms (100.00%) | Memory: 52.8 MB (91.29%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/search-a-2d-matrix.js
@@ -4,22 +4,43 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-  let n = matrix.length;
-  let m = matrix[0].length; 
-  let low = 0; 
-  let high = (m * n - 1);
+  let m=matrix.length;
+  let n=matrix[0].length;  
 
-  while (low <= high) {
-    let mid = Math.floor((low + high) / 2);
-    let row = Math.floor(mid / m);
-    let col = mid % m;
+  let startRow=0;
+  let endRow=m-1;
 
-    if (matrix[row][col] < target) {
-      low = mid + 1;
-    } else if (matrix[row][col] > target) {
-      high = mid - 1;
-    } else {
-      return true;
+  function searchInCol(mat, tar, midRow){
+    let start=0;
+    let end=n-1;
+
+    while(start <= end){
+        let mid= Math.floor((start + end)/2)
+
+        if(mat[midRow][mid] === target){
+            return true;
+        }
+        else if(mat[midRow][mid] < tar){
+            start = mid+1;
+        }else{
+            end = mid-1;
+        }
+    }
+    return false;
+  }
+
+  while(startRow <= endRow){
+    let midRow=Math.floor((startRow + endRow) / 2);
+
+    if(matrix[midRow][0] <= target && matrix[midRow][n-1] >= target){
+        // Find the row
+        return searchInCol(matrix, target, midRow);
+    }
+    else if(target >= matrix[midRow][n-1]){
+        startRow=midRow+1;
+    }
+    else{
+        endRow=midRow-1;
     }
   }
 
