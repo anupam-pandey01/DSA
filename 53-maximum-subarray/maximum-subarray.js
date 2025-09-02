11d1d9e9574f91b4caa753348adab571554f681d
Anupam Pandey <pandeyanupam180@gmail.com>
2025-09-02 10:53:28 +0530

    Time: 1 ms (90.78%) | Memory: 63.8 MB (41.01%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -3,14 +3,15 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let res = -Infinity;
+    let maxSum= - Infinity;
     let currSum = 0;
+
     for(let i=0; i<nums.length; i++){
-        currSum += nums[i];
-        res = Math.max(res, currSum);
+        currSum+=nums[i];
+        maxSum = Math.max(currSum, maxSum);
         if(currSum < 0){
-            currSum = 0;
+            currSum = 0
         }
     }
-    return res;
+    return maxSum;
 };
