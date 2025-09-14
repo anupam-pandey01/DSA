5c01bf8e06ed5fdf03a64d244495ced7f07a3cb0
Anupam Pandey <pandeyanupam180@gmail.com>
2025-09-15 00:51:17 +0530

    Time: 3 ms (25.64%) | Memory: 18.1 MB (38.47%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        def subset(nums, i, comb, ans):
+            nums.sort()
+            if(i == len(nums)):
+                ans.append(comb.copy())
+                return
+
+            comb.append(nums[i])
+            subset(nums, i+1, comb, ans)
+            comb.pop()
+
+            idx = i + 1
+
+            while(idx < len(nums) and nums[idx] == nums[idx - 1]):
+                idx += 1
+            
+            subset(nums, idx, comb, ans)
+
+        subset(nums, 0, [], ans)
+
+        return ans
+            
