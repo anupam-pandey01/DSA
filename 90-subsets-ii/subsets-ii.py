47eddfb569f9ce4778803055fd7f1cbc7ce1c1a3
Anupam Pandey <pandeyanupam180@gmail.com>
2025-09-15 15:33:50 +0530

    Time: 4 ms (13.90%) | Memory: 17.7 MB (97.65%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -9,16 +9,16 @@ class Solution:
 
             comb.append(nums[i])
             subset(nums, i+1, comb, ans)
-            comb.pop()
 
-            idx = i + 1
-
+            idx = i+1
             while(idx < len(nums) and nums[idx] == nums[idx - 1]):
                 idx += 1
             
+            comb.pop()
             subset(nums, idx, comb, ans)
 
         subset(nums, 0, [], ans)
-
         return ans
+        
             
+
