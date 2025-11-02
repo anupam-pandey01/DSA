6fa533e9b6044815df648256f7cf8faf718547b1
Anupam Pandey <pandeyanupam180@gmail.com>
2025-11-02 17:13:54 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (73.88%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.py
+++ 328-odd-even-linked-list/odd-even-linked-list.py
@@ -8,25 +8,38 @@ class Solution:
         if (head is None or head.next is None):
             return head
         
-        order = []
-        temp = head
+        odd = head
+        even = head.next
+        even_head = even
 
-        while(temp != None):
-            order.append(temp.val)
-            temp = temp.next.next if temp.next else None
-        
+        while(even != None and even.next != None):
+            odd.next = odd.next.next
+            odd = odd.next
+            even.next = even.next.next
+            even = even.next
 
-        temp = head.next
-
-        while(temp != None):
-            order.append(temp.val)
-            temp = temp.next.next if temp.next else None
-        
-        temp = head
-        i = 0
-        while(temp != None):
-            temp.val = order[i]
-            i+=1
-            temp = temp.next
-        
+        odd.next = even_head
         return head
+        
+        # order = []
+        # temp = head
+
+        # while(temp != None):
+        #     order.append(temp.val)
+        #     temp = temp.next.next if temp.next else None
+        
+
+        # temp = head.next
+
+        # while(temp != None):
+        #     order.append(temp.val)
+        #     temp = temp.next.next if temp.next else None
+        
+        # temp = head
+        # i = 0
+        # while(temp != None):
+        #     temp.val = order[i]
+        #     i+=1
+        #     temp = temp.next
+        
+        # return head
