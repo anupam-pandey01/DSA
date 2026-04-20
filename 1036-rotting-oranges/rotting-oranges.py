b465dbffd25569af4103145ab207fa98bed5ae52
Anupam Pandey <pandeyanupam180@gmail.com>
2026-04-20 10:23:06 +0530

    Time: 4 ms (35.67%) | Memory: 19.2 MB (81.58%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,44 +1,47 @@
 from copy import deepcopy
-from collections import deque 
+from collections import deque
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        grid_copy = deepcopy(grid)
         rows = len(grid)
         cols = len(grid[0])
 
+        grid_copy = deepcopy(grid)
+        queue = deque([])
         fresh_org = 0
-        queue  = deque([])
 
-        for r in range(rows):
-            for c in range(cols):
-                if grid_copy[r][c] == 2:
-                    queue.append((r, c))
-                
-                if grid_copy[r][c] == 1:
+        for i in range(rows):
+            for j in range(cols):
+                if grid_copy[i][j] == 1:
                     fresh_org += 1
+                if grid_copy[i][j] == 2:
+                    queue.append((i, j))
         
         minutes = 0
+        
         while len(queue) != 0 and fresh_org > 0:
             minutes += 1
             total_rotten = len(queue)
 
             for _ in range(total_rotten):
                 i, j = queue.popleft()
-                for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                    new_i, new_j = i + dx, j + dy
+                for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    ni = i + dx
+                    nj = j + dy
 
-                    if new_i < 0 or new_i >= rows or new_j < 0 or new_j >= cols:
+                    if ni < 0 or ni >= rows or nj < 0 or nj >= cols:
                         continue
                     
-                    if grid_copy[new_i][new_j] == 0 or grid_copy[new_i][new_j] == 2:
+                    if grid_copy[ni][nj] == 2 or grid_copy[ni][nj] == 0:
                         continue
                     
+                    grid_copy[ni][nj] = 2
+                    queue.append((ni, nj))
                     fresh_org -= 1
-                    grid_copy[new_i][new_j] = 2
-                    queue.append((new_i, new_j))
-        
+
         if fresh_org > 0:
             return -1
         
         return minutes
+            
+         
