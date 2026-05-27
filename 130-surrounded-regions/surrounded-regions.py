b1225d92f6631e8d1e780ea200557e18d3c01a10
Anupam Pandey <pandeyanupam180@gmail.com>
2026-05-27 11:11:42 +0530

    Time: 2 ms (87.50%) | Memory: 22.4 MB (59.10%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -1,52 +1,41 @@
+from collections import deque
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         rows = len(board)
         cols = len(board[0])
 
         visited = [[0 for _ in range(cols)] for _ in range(rows)]
+        queue = deque([])
 
-        def marker(r, c):
-            n_row = r
-            n_col = c
-            if n_row < 0 or n_row >= rows or n_col < 0 or n_col >= cols:
-                return
-
-            if visited[n_row][n_col] == 1 or board[n_row][n_col] != "O":
-                return
-                
-
-            visited[n_row][n_col] = 1
-
-            marker(n_row - 1, n_col)
-            marker(n_row + 1, n_col)
-            marker(n_row, n_col + 1)
-            marker(n_row, n_col - 1)
-
-
-        # Top
-        for i in range(cols):
-            if board[0][i] == "O":
-                marker(0, i)
-
-        # Bottom row
-        for j in range(cols):
-            if board[rows - 1][j] == "O":
-                marker(rows - 1, j)  
-
-        # Right 
-        for i in range(1, rows - 1):
-            if board[i][cols - 1] ==  'O':
-                marker(i, cols - 1)
+        for c in range(cols):
+            if board[0][c] == "O":
+                queue.append((0, c))
+            if board[rows - 1][c] == "O":
+                queue.append((rows - 1, c))
         
-        # Left
-        for j in range(1, rows - 1):
-            if board[j][0] == "O":
-                marker(j, 0)
+        for r in range(1, rows - 1):
+            if board[r][0] == "O":
+                queue.append((r, 0))
+            
+            if board[r][cols - 1] == "O":
+                queue.append((r, cols - 1))
+        
+        while queue:
+            r, c = queue.popleft()
+            visited[r][c] = 1
+            for dx, dy in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+                nr = r + dx
+                nc = c + dy
 
+                if nr < 0 or nr >= rows or nc < 0 or nc >= cols:
+                    continue
+
+                if visited[nr][nc] == 1 or board[nr][nc] == "X":
+                    continue
+                
+                queue.append((nr, nc))
         
         for i in range(rows):
             for j in range(cols):
                 if visited[i][j] == 0 and board[i][j] == "O":
                     board[i][j] = "X"
-        
-
