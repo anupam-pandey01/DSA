256d9eec1b61cbf1c9d0270e0f90ce7e3514e516
Anupam Pandey <pandeyanupam180@gmail.com>
2026-07-25 00:48:56 +0530

    Time: 19 ms (5.90%) | Memory: 19.6 MB (76.50%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,31 +1,29 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s) - 1
+
         def isAlphabet(ch):
-            if (
-                (ch >= "0" and ch <= "9") or
-                (ch >= "a" and ch <= "z") or
-                (ch >= "A" and ch <= "Z")
-            ):
-                return True
-
+            if ( (ch >= "a" and ch <= "z") or
+                (ch >= "A" and ch <= "Z") or
+                (ch >= "0" and ch <= "9") ):
+               return True
+            
             return False
-        
-        st = 0
-        end = len(s) - 1
 
-        while st < end:
-            if not isAlphabet(s[st]):
-                st+=1
+        while i < j:
+            if not isAlphabet(s[i]):
+                i += 1
+                continue
+
+            if not isAlphabet(s[j]):
+                j -= 1
                 continue
             
-            if not isAlphabet(s[end]):
-                end -= 1
-                continue
-            
-            if s[st].lower() != s[end].lower():
+            if s[i].lower() != s[j].lower():
                 return False
             
-            st += 1
-            end -= 1
-        
+            i += 1
+            j -= 1
         return True
+            
